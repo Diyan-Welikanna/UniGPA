01506076afe30b127604805197e4b8688c41d00c
Diyan Thimeesha <thimeesharandika@gmail.com>
2026-03-02 21:20:03 +0530

    Fix admin template editor: remove duplicate SessionProvider causing stuck loading

--- src/app/admin/degrees/[id]/layout.tsx
+++ src/app/admin/degrees/[id]/layout.tsx
@@ -1,7 +1,5 @@
 export const dynamic = 'force-dynamic';
 
-import SessionWrapper from '@/app/select-degree/SessionWrapper';
-
 export default function AdminDegreeLayout({ children }: { children: React.ReactNode }) {
-  return <SessionWrapper>{children}</SessionWrapper>;
+  return <>{children}</>;
 }
